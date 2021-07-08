eebef6ec74f49bfb763dfb0a7b46e8dc641d96c1
nabil-bali <goualnabil691@gmail.com>
2021-07-08 13:44:24 +0200

    firt commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var path = require('path');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var open = require('gulp-open');
+
+var Paths = {
+  HERE: './',
+  DIST: 'dist/',
+  CSS: './assets/css/',
+  SCSS_TOOLKIT_SOURCES: './assets/scss/material-kit.scss',
+  SCSS: './assets/scss/**/**'
+};
+
+gulp.task('compile-scss', function() {
+  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write(Paths.HERE))
+    .pipe(gulp.dest(Paths.CSS));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(Paths.SCSS, gulp.series('compile-scss'));
+});
+
+// set chrome navigator
+var options = {
+  app: 'chrome'
+};
+
+gulp.task('open', function() {
+  gulp.src('template.html')
+    .pipe(open(options));
+});
+
+gulp.task('open-app', gulp.parallel('open', 'watch'));
