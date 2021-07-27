5df94806ad0fe085b2d8af270a5a6159e091e145
nabil-bali <goualnabil691@gmail.com>
2021-07-27 14:18:25 +0200

    update 27/07

---
+++ assets/js/onscroll.js
@@ -0,0 +1,23 @@
+// let exp = document.getElementById("exp");
+
+
+// exp.style.opacity = 1;
+
+
+
+// var observer = new IntersectionObserver(function(entries) {
+// 	if(entries[0].isIntersecting === true)
+// 		alert('Element is fully visible in screen');
+// }, { threshold: [1] });
+
+// observer.observe(document.getElementById("exp"));
+
+
+// // window.onscroll = function () {
+// //     document.getElementById("test").innerHTML = window.scrollY;
+// // }
+
+// // if (exp.offsetTop > window.scrollY) {
+// //     exp.style.opacity = 0;
+// //     exp.animate({opacity:0}, 500)
+// // }
